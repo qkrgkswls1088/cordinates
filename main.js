37103740bee85206d62f656e862b7d3ac4a051ee
qkrgkswls1088 <qkrgkswls1088@gmail.com>
2021-11-01 11:30:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-const horizontal = document.querySelector('horizontal');
-const vertical = document.querySelector('vertical');
-const target = document.querySelector('target');
-const tag = document.querySelector('tag');
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
 
 addEventListener('load' , function () {
   const targetRect = target.getBoundingClientRect();
   // console.log(targetRect);
   const targetHalfWidth = targetRect.width / 2;
   const targetHalfHeight = targetRect.height / 2;
+  let curX = 0;
+  let curY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
 
   document.addEventListener('mousemove' , (event) => {
-    const x = event.clientX
-    const y = event.clientY
-    console.log(x + "px" + y + "px");
-    console.log(`${x} px ${y} px백틱키사용`);
-
-    horizontal.style.transform = `translateY(${y}px)`;
-    vertical.style.transform = `translateY(${x}px)`;
-    target.style.transform = `translate(${x - targetHalfWidth}px , ${y - targetHalfHeight}px)`;
-    tag.style.transform = `translate(${x}px, ${y}px)`;
-    tag.innerHTML = `${x}px ${y}px`;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    console.log(mouseX + "px" + mouseY + "px");
+    console.log(`${mouseX} px ${mouseY} px백틱키사용`);
   });
 
+    function animate() {
+      requestAnimationFrame(animate);
+      curX += (mouseX - curX) * 0.01;
+      curY += (mouseY - curY) * 0.01;
+    
+
+    horizontal.style.transform = `translateY(${curY}px)`;
+    vertical.style.transform = `translateX(${curX}px)`;
+    target.style.transform = `translate(${curX - targetHalfWidth}px , ${curY - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${curX}px, ${curY}px)`;
+    tag.innerHTML = `${Math.ceil(curX)}px ${Math.ceil(curY)}px`;}
+
+  animate();
 });
